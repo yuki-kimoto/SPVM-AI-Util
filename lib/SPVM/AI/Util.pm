f5a48953cb62d8de048e7b3af382f3d4221609dd
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-09-26 08:52:02 +0900

    first commit

---
+++ lib/SPVM/AI/Util.pm
@@ -0,0 +1,109 @@
+package SPVM::AI::Util;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+SPVM::AI::Util - The great new SPVM::AI::Util!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use SPVM::AI::Util;
+
+    my $foo = SPVM::AI::Util->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Yuki Kimoto, C<< <kimoto.yuki at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-spvm-ai-util at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=SPVM-AI-Util>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc SPVM::AI::Util
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=SPVM-AI-Util>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/SPVM-AI-Util>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/SPVM-AI-Util>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/SPVM-AI-Util>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2020 by Yuki Kimoto.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of SPVM::AI::Util
